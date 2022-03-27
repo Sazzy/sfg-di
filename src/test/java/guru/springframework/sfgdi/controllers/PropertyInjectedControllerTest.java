99be85b1b5faf34cf9cd58df17ad14f48656dbad
Eugene Sazonov <saz.vit@gmail.com>
2022-03-27 14:11:05 +0200

    Qualifiers usage example

--- src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -1,19 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.PropertyGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 	PropertyInjectedController controller;
 
 	@BeforeEach
 	void setUp() {
-		 controller = new PropertyInjectedController();
+		controller = new PropertyInjectedController();
 
-		 controller.greetingService = new GreetingServiceImpl();
+		controller.greetingService = new PropertyGreetingService();
 	}
 
 	@Test
