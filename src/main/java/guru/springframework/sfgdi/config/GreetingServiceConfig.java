905a1f22c8b4ee3d6fc72e9c8493ac08c189d92d
Eugene Sazonov <saz.vit@gmail.com>
2022-04-03 14:01:27 +0200

    Primary beans and profiles in configuration file implementation example

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,32 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyGreetingService;
-import guru.springframework.sfgdi.services.SetterGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+	@Profile({"EN"})
+	@Bean("i18nService")
+	I18nEnglishGreetingService i18nEnglishGreetingService() {
+		return new I18nEnglishGreetingService();
+	}
+
+	@Profile({"ES", "default"})
+	@Bean("i18nService")
+	I18nSpanishGreetingService i18nSpanishGreetingService() {
+		return new I18nSpanishGreetingService();
+	}
+
+	@Primary
+	@Bean
+	PrimaryGreetingService primaryGreetingService() {
+		return new PrimaryGreetingService();
+	}
+
 	@Bean
 	ConstructorGreetingService constructorGreetingService() {
 		return new ConstructorGreetingService();
